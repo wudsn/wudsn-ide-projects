1bb43281533d005b891e8021f5598f5eab01f87d
Peter Dell <peter.dell@web.de>
2022-09-02 21:03:15 +0200

    Update Part 15

--- Atari800/Tutorial/Part 15 - Raster Music Tracker Modules/Part15.asm
+++ Atari800/Tutorial/Part 15 - Raster Music Tracker Modules/Part15.asm
@@ -3,49 +3,53 @@
 ;
 ; http://raster.infos.cz/atari/rmt/rmt.htm#download
 ;
-; http://asma.atari.org/
-;
 ; C:\jac\wudsn\Tools\ASM\MADS\examples\players\rmt_player_relocator
+
+
+rtclok	= $12
+vcount	= $d40b
 
 	org $2400
 
-rtclok	= $12
-
 	.proc music
-STEREOMODE = 0
-init	= RASTERMUSICTRACKER+0
-play	= RASTERMUSICTRACKER+3
-stop	= RASTERMUSICTRACKER+9
+STEREOMODE	equ 0				;0 => compile RMTplayer for mono 4 tracks
+init_song = RASTERMUSICTRACKER+0
+play	  = RASTERMUSICTRACKER+3
 
 	icl "music.feat"
 
-	icl "rmt_player.a65"			;include RMT player routine
+player
+	icl "rmt_player.asm"			;include RMT player routine
 	
-	icl 'rmt_relocator.mac'
+	icl 'rmt_relocator.asm'	
+
 module
 	rmt_relocator 'music.rmt' module	;include music RMT module
-
+	
 	.endp
 
 	.proc main
-	ldx #<music.module			;low byte of RMT module to X reg
-	ldy #>music.module			;Hi byte of RMT module to Y reg
-	lda #0					;Starting song line 0-255 to A reg
-	jsr music.init				;Init RMT player for module & song
-
+	
+	ldx #<music.module
+	ldy #>music.module
+	lda #0
+	jsr music.init_song
+	
 main_loop
 	jsr wait
+	mva #$34 $d01a
 	jsr music.play
-	jmp main_loop
+	mva #$00 $d01a
 
+	jmp main_loop
+	
 	.proc wait
-	lda rtclok+2
-loop	cmp rtclok+2
-	beq loop
+loop	lda vcount
+	cmp #$20
+	bne loop
 	rts
 	.endp
 
 	.endp
 	
 	run main
-
