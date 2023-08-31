a39a11e1d5eeb6a9a5a62539878a535bd57e29b7
Peter Dell <peter.dell@web.de>
2023-08-31 15:09:00 +0200

    Update Example.sh for macOS

--- Atari800/Pascal/Example/Example.sh
+++ Atari800/Pascal/Example/Example.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 tools=~/jac/wudsn/Tools
-mads=$tools/ASM/MADS/mads.macosx-x86-64
-mp=$tools/PAS/MP/mp.macosx-x86-64
-base=$tools/PAS/base
+mads=$tools/ASM/MADS/mads.macos-x86-64
+mp=$tools/PAS/MP/mp.macos-x86-64
+lib=$mp/lib
+base=$mp/base
 
 set -e
 
-$mp Example.pas
+$mp Example.pas -ipath:$lib
 $mads Example.a65 -x -i:$base -o:Example.xex
 open Example.xex
