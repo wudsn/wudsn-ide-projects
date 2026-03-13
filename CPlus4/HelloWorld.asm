bd2098e038309880ab79b7e0b5a7f0d8973f1f1b
Peter Dell <peter.dell@web.de>
2026-03-13 08:05:52 +0100

    Update Commodore Plus/4 Example Project

--- CPlus4/HelloWorld.asm
+++ CPlus4/HelloWorld.asm
@@ -1,4 +1,12 @@
 //	WUDSN IDE example KickAss source file for Commodore Plus/4
+//
+//     In the IDE preferences for "Editors/File Associations":
+//	- Associate ".asm" with "KICKASS Editor"
+//
+//     In the IDE preferences for "Assmemblers/C64":
+//	- Select "Vice" as "Default Application to Open File".
+//	- Configure the path to ...\VICE\bin\xplus4.exe".
+//	- Select the "Wait for end of application" checkbox.
 
 .pc = $1001 "Basic Upstart"
 :BasicUpstart(start) // 10 sys$1010
@@ -13,4 +21,4 @@ loop:         lda mytext,x
               bne loop
               brk
 
-mytext: .text "HELLO WORLD"
+mytext: .text "HELLO WORLD!"
